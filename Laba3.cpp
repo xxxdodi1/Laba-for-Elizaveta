5a346820e2009062da88cb395e3b3feafb0fb320
xxxdodi1 <114001082+xxxdodi1@users.noreply.github.com>
2023-04-20 20:55:13 +0300

    Update Laba3.cpp

--- Laba3.cpp
+++ Laba3.cpp
@@ -7,10 +7,12 @@
 #include <string>
 #include <algorithm>
 #include <iterator>
+#include <cstdlib>
+
 
 using namespace std;
 
-
+string str;
 
 void fun(map<string, int>mp) {
     for (auto elem : mp)
@@ -21,80 +23,59 @@ void fun(map<string, int>mp) {
 }
 
 
+
 int main()
 {
-
+    setlocale(LC_ALL, "ru");
     map <string, int> mp;
     ifstream fin;
     fin.open("text.txt");
-
+    int abc = 0;
     if (fin.is_open())
     {
-        string str;
+        
         int i = 1;
         while (!fin.eof())
         {
             str = "";
             fin >> str;
-
+            size_t pos = 0;
+            const char del = ' ';
+            while (pos != str.npos) {
+                pos = str.find_first_not_of(del, pos);
+                if (pos == str.npos) break;
+                if(str[pos]=='a'|| str[pos] == 'b'||str[pos] == 'c') {
+                    std::cout << str[pos] << " "<<del;
+                    pos = str.find(del, pos + 1);
+                    abc++;
+                }
+                else {
+                    str[pos] << del;
+                    pos = str.find(del, pos + 1);
+                }
+            }
+            
+            
+            
             if (mp.count(str) == 0)
             {
+           
                 mp.insert(make_pair(str, 1));
             }
             else
             {
                 mp[str]++;
             }
-
+            
         }
+        
+            
+        
     }
 
     fun(mp);
-
+    cout << "Кол-во слов начинающихся на a,b,c-" << abc;
 
     fin.close();
-
-    
-
-
-    /*
-    vector<int> numbers;
-
-    size_t numbers_count;
-    cin >> numbers_count;
-    
-    // Размер вектора остаётся нулевым, меняется только резерв:
-    numbers.reserve(numbers_count);
-
-    for (size_t i = 0; i != numbers_count; ++i)
-    {
-        numbers[i] = rand() % 100;
-
-    }
-    for (size_t i = 0; i < numbers_count; i++)
-    {
-        if (i % 2 == 0)//не четн. тк индексы начинаются с 0
-        {
-            if (numbers[i] > numbers[i + 2])
-            {
-                swap(numbers[i], numbers[i + 2]);
-            }
-            else
-            {
-                if (numbers[i] > numbers[i + 2])
-                {
-                    swap(numbers[i], numbers[i + 2]);
-                }
-            }
-        }
-        for (size_t i = 0; i < numbers_count; i++)
-        {
-            cout << numbers[i] << " ";
-            cout << "\n";
-        }
-        
-        
-    }
-    */
     return (0);
 }
